10868f971cf59a5033400762e4e4f8d9aabf1464
Bai Li <bai.li@noaa.gov>
2023-08-11 10:54:09 -0400

    fix: install ASSAMC in testthat test

--- tests/testthat/test-data_assamc.R
+++ tests/testthat/test-data_assamc.R
@@ -37,6 +37,15 @@ test_that("data assamc has correct structure", {
 })
 
 test_that("assamc data are unchanged", {
+  # Install ASSAMC package
+  pkg_all <- installed.packages()[, "Package"]
+  if (!"ASSAMC" %in% pkg_all) {
+    pak::pak(
+      "Bai-Li-NOAA/Age_Structured_Stock_Assessment_Model_Comparison"
+    )
+  }
+  library("ASSAMC")
+
   #' @description Testing that the ASSAMC package version is unchanged
   expect_snapshot(x = packageVersion("ASSAMC"))
 
